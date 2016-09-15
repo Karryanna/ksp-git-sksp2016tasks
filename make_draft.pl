4cfd588266bce594e3af5fbee88b31e0e061eb90
Karryanna <karry@karryanna.cz>
2016-09-15 16:25:56 +0200

    make_draft: Stejné ovlivnění jmen souborů jako v make_test

--- make_draft.pl
+++ make_draft.pl
@@ -2,10 +2,16 @@ use common::sense;
 
 say STDERR "Tento skript by rád vytexal soubor";
 
-while (<>)
+my $out = $ARGV[0];
+$out =~ s/\.chosen/.dratf/;
+open (my $infile, "<", $ARGV[0]) or die("Nemůžu otevřít vstupní soubor");
+open (my $outfile, ">", $out) or die("Nemůžu otevřít výstupní soubor");
+while (<$infile>)
 {
   chomp;
   # Rozdělíme slovo na tabulátorech
   my ($target, $source, $comment) = split "\t";
-  say $target;
+  say $outfile $target;
 }
+close ($infile);
+close ($outfile);
