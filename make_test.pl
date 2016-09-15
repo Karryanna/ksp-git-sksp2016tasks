f5de41a80ba1f143b749d23d7ad0d564be62596f
Karryanna <karry@karryanna.cz>
2016-09-15 16:20:37 +0200

    make_test: Oprava překlepu

--- make_test.pl
+++ make_test.pl
@@ -3,13 +3,13 @@ use common::sense;
 say STDERR "Tento skript by rád přeložil vytexaný soubor, ale kdo slibuje, že lidi mají TeX?";
 
 my $out = $ARGV[0];
-$out =~ s/\.draft/.test/;
+$out =~ s/\.draft/.tets/;
 open (my $infile, "<", $ARGV[0]) or die("Nemůžu otevřít vstupní soubor");
 open (my $outfile, ">", $out) or die("Nemůžu otevřít výstupní soubor");
 while (<$infile>)
 {
   chomp;
-  # Zadané slovíčko vypiusjeme i s prostorem na cizojazyčnou verzi
+  # Zadané slovíčko vypisujeme i s prostorem na cizojazyčnou verzi
   say $outfile "$_: ___";
 }
 close ($infile);
